6b994784c0d70d4b42d246d17a39ba296d6203e2
Raymond Mojica <rm@M12020.home>
2022-11-22 13:37:41 -0500

    setting up navbar and landing page

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <a href="index.html">
+          <i classname="fas fa-code"></i>SocialApp
+        </a>
+      </h1>
+      <ul>
+        <li>
+          <a href="profiles.html">Developer</a>
+        </li>
+        <li>
+          <a href="register.html">Register</a>
+        </li>
+        <li>
+          <a href="login.html">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
